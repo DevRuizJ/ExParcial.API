81814ab9e1642cc44f68d4d6aef860562485b0be
DevJARM <repo.ruiz.javier@gmail.com>
2024-05-18 16:08:16 -0500

    init project

---
+++ ExParcial.API/Data/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExParcial.API.Models;
+
+public partial class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Genre { get; set; }
+
+    public int? ReleaseYear { get; set; }
+}
